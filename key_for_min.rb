db46ed4f75d215ff5fe59d1507a08be71618b4ef
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 02:41:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   result = nil
-  if !name_hash.empty? do
+  if !name_hash.empty?
     arr = []
     name_hash.each do |x, i|
       arr << i
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
     i = 1
     min = arr[0]
     until i == arr.size do
-      if arr[i] < min do
+      if arr[i] < min
         min = arr[i]
       end
       i += 1
